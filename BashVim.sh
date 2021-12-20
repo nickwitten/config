e2bfaa8628ab173477fc2f1ab0d30b054f66d798
Nick Witten <nwitt12@gmail.com>
2021-12-20 11:51:37 -0500

    Create vim backup dirs

--- BashVim.sh
+++ BashVim.sh
@@ -2,3 +2,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp ~/config/.vimrc ~/.vimrc
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/swap
--- WindowsVim.ps1
+++ WindowsVim.ps1
@@ -1,11 +1,16 @@
 # If unable to run script, issue this
 # set-ExecutionPolicy Unrestricted
 
+mkdir ~/.vim
 
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
 
 copy ~/config/_vimrc ~/_vimrc
 copy ~/config/.vimrc ~/.vimrc
+
+mkdir ~/.vim/backup
+mkdir ~/.vim/undo
+mkdir ~/.vim/swap
 
 # git clone https://github.com/morhetz/gruvbox.git "~\vimfiles\pack\default\start\gruvbox"
