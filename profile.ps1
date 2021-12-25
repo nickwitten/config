a0537528480b503dbabe2c4f464d51be14358c9e
Nick Witten <nwitt12@gmail.com>
2021-12-24 19:47:39 -0500

    Python config in powershell

--- profile.ps1
+++ profile.ps1
@@ -6,3 +6,47 @@ function Prompt {
   $out += "$([char]27)]9;9;`"$loc`"$([char]27)\"
   return $out
 }
+
+function ReloadPath {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    return
+}
+
+########## PYTHON ###################
+$PythonInstallPath = "$env:LOCALAPPDATA\Programs\Python"
+$DefaultPythonVersionNoDot = "38"
+function AddPythonToPath {
+    $env:Path = "$PythonInstallPath\Python$DefaultPythonVersionNoDot\Scripts;$env:Path"
+    $env:Path = "$PythonInstallPath\Python$DefaultPythonVersionNoDot;$env:Path"
+    # Add each python installation to path
+    Get-ChildItem $PythonInstallPath | Where-Object { $_.name -like "Python*" } | ForEach-Object {
+        if (";$env:Path;" -notlike "*;$PythonInstallPath\$_.name;*") {
+            $env:Path += ";" + $_.FullName
+            $env:Path += ";" + $_.FullName + "\Scripts"
+        }
+    }
+}
+AddPythonToPath
+
+function InstallPython {
+
+    param ($PythonVersion)
+
+    $PythonVersionNoDot = $PythonVersion -replace '[.]',''
+    $InstallDir = "$PythonInstallPath\Python$PythonVersionNoDot"
+
+    $pycmd = "choco install python3 --version $PythonVersion --side-by-side -y --params `"/installdir:$InstallDir`""
+
+    Invoke-Expression "$pycmd"
+
+    Copy-Item $InstallDir\python.exe $InstallDir\python$PythonVersionNoDot.exe
+
+    $env:Path += ";$InstallDir"
+
+    Invoke-Expression "python$PythonVersionNoDot -m ensurepip --upgrade"
+
+    AddPythonToPath
+    echo "Add to path: "
+    echo "$InstallDir"
+    echo "$InstallDir\Scripts"
+}
--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,20 @@
 # If unable to run script, issue this
 # Set-ExecutionPolicy Unrestricted
+. ~/config/profile.ps1
 
 ######### Packages ################
+
+# Choco
 try {
     choco --version | Out-Null
 }
 catch [System.Management.Automation.CommandNotFoundException] {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     echo "Installed Chocolatey"
+    choco feature enable -n useRememberedArgumentsForUpgrades
 }
+
+# Git
 try {
     git --version | Out-Null
 }
@@ -16,12 +22,30 @@ catch [System.Management.Automation.CommandNotFoundException] {
     choco install git -y
     echo "Installed Git"
 }
+
+# Vim
 try {
     vim --version | Out-Null
 }
 catch [System.Management.Automation.CommandNotFoundException] {
     choco install vim -y
     echo "Installed Vim"
+}
+
+# Python 3.10
+try {
+    python310 --version | Out-Null
+}
+catch [System.Management.Automation.CommandNotFoundException] {
+    InstallPython "3.10"
+}
+
+# Python 3.8
+try {
+    python38 --version | Out-Null
+}
+catch [System.Management.Automation.CommandNotFoundException] {
+    InstallPython "3.8"
 }
 
 
@@ -35,11 +59,11 @@ if (-Not (Test-Path -Path $vimplugpath)) {
 Copy-Item ~/config/_vimrc ~/_vimrc -Force | Out-Null
 
 
-#### PowerShell Configuration ######
+######## PowerShell ################
 New-Item -ItemType File -Path $profile -Force | Out-Null
-echo "~/config/profile.ps1" > $profile
+echo ". ~/config/profile.ps1" > $profile
 
 
-### Windows Terminal ###############
+###### Windows Terminal ############
 Copy-Item ~\config\windowsterm.json ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 
