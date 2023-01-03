6f22029e3ecb6fb630fa2af5cd8bde5503f4c9ca
Nick Witten <nwitt12@gmail.com>
2023-01-02 23:44:41 -0500

    big changes to simplify configure.sh, fzf paste remap

--- configure.sh
+++ configure.sh
@@ -1,88 +1,49 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 
-command_exists() {
-    command -v "$@" >/dev/null 2>&1
-}
+# Configuration file links
+link ~/config/.vimrc ~/.vimrc 2> /dev/null
+link ~/config/.tmux.conf ~/.tmux.conf 2> /dev/null
+link ~/config/.zshrc ~/.zshrc 2> /dev/null
+link ~/config/.bashrc ~/.bashrc 2> /dev/null
+mkdir -p ~/.config/nvim
+link ~/config/init.vim ~/.config/nvim/init.vim 2> /dev/null
 
-contains_match() {
-    grep -q "$2" $1 >/dev/null 2>&1
-}
-
-##### Requirements #####
-if ! command_exists curl || ! command_exists git; then
-    echo "Unable to configure, missing curl or git"
-    exit 1
-fi
-
-##### BASH #####
-BASHRC_CMD='source ~/config/.bashrc'
-if ! contains_match ~/.bashrc "${BASHRC_CMD}"; then
-    echo ${BASHRC_CMD} >> ~/.bashrc
-    echo "BASH CONFIGURED"
-fi;
-
-##### ZSH #####
-ZSHRC_CMD='source ~/config/.zshrc'
-if command_exists zsh; then
-    if ! [ -d $HOME/.oh-my-zsh ]; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        if [ -f $HOME/.zshrc.pre-oh-my-zsh ]; then
-            mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
-        else
-            rm $HOME/.zshrc
-        fi
-        mkdir -p "$HOME/.oh-my-zsh/custom/themes"
-        git clone https://github.com/sindresorhus/pure.git "$HOME/.oh-my-zsh/custom/themes/pure"
-    fi
-    if ! contains_match ~/.zshrc "${ZSHRC_CMD}"; then
-        echo ${ZSHRC_CMD} >> ~/.zshrc
-	echo "ZSH CONFIGURED"
-    fi
-fi
-
-##### VIM #####
-VIMRC_CMD='source ~/config/.vimrc'
-VIMPLUG_LOC=~/.vim/autoload/plug.vim
-if command_exists vim; then
-    if ! contains_match ~/.vimrc "${VIMRC_CMD}"; then
-        echo -e ${VIMRC_CMD} >> ~/.vimrc;
-	echo -e "VIM CONFIGURED"
-    fi
-    if [ ! -f ${VIMPLUG_LOC} ]; then
-        curl -fLo ${VIMPLUG_LOC} --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        echo -e "VIMPLUG INSTALLED"
-    fi
-fi
-
-##### NVIM #####
-NVIMRC_LOC="${HOME}/.config/nvim"
-if command_exists nvim; then
-    if ! contains_match "${NVIMRC_LOC}/init.vim" "${VIMRC_CMD}"; then
-	mkdir -p ${NVIMRC_LOC}
-	echo -e "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath=&runtimepath" >> "${NVIMRC_LOC}/init.vim";
-	echo -e ${VIMRC_CMD} >> "${NVIMRC_LOC}/init.vim";
-	echo -e "NVIM CONFIGURED"
-    fi
-fi
-
-##### TMUX #####
-TMUX_SOURCE='source ~/config/.tmux.conf'
-if command_exists tmux; then
-    if ! contains_match ~/.tmux.conf "${TMUX_SOURCE}"; then
-        echo "${TMUX_SOURCE}" >> ~/.tmux.conf;
-	echo -e "TMUX CONFIGURED"
-    fi
-fi
-
-##### GIT #####
+##### Git #####
 if ! git config --list --global | grep user.email > /dev/null; then
     echo -e;
     read -p "Would you like to configure git for personal use? [y/n]" USER_CONFIRM
     if [ "${USER_CONFIRM}" = "y" ]; then
-	git config --global user.email "nwitt12@gmail.com"
-	git config --global user.name "Nick Witten"
-	echo -e "GIT CONFIGURED"
+        git config --global user.email "nwitt12@gmail.com"
+        git config --global user.name "Nick Witten"
+        echo -e "GIT CONFIGURED"
     fi
+fi
+
+##### Requirements #####
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+if ! command_exists curl; then
+    echo "Missing curl, unable to install zsh and vim plugins"
+    exit 1
+fi
+
+# Install zsh plugins
+if ! [ -d $HOME/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    if [ -f $HOME/.zshrc.pre-oh-my-zsh ]; then
+        mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
+    else
+        rm $HOME/.zshrc
+    fi
+    mkdir -p "$HOME/.oh-my-zsh/custom/themes"
+    git clone https://github.com/sindresorhus/pure.git "$HOME/.oh-my-zsh/custom/themes/pure"
+fi
+
+# Install vimplug
+VIMPLUG_LOC=~/.vim/autoload/plug.vim
+if [ ! -f ${VIMPLUG_LOC} ]; then
+    curl -fLo ${VIMPLUG_LOC} --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
